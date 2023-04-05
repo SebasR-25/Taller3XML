c4c56c3f2d0dd0cd4c337bccb18b072ba9470464
SebasR-25 <seruprisatur2703@gmail.com>
2023-04-05 18:37:06 -0500

    Se agfregó en la vista el método para el menú principal

---
+++ src/view/View.java
@@ -0,0 +1,25 @@
+package view;
+import java.util.Scanner;
+
+public class View{
+    private Scanner input;
+
+    public View(){
+        input = new Scanner(System.in);
+    }
+    public void showMessage(String message){
+		System.out.println(message);
+	}
+    public double readDouble(String message){
+		this.showMessage(message);
+		return input.nextDouble();
+	}
+    public short readShort(String message){
+		this.showMessage(message);
+		return input.nextShort();
+	}
+    public short readMenu(){
+        String menuText = "1.Añadir habitación"+"\n"+"2. Ingresar paciente"+"\n"+"3.  Mostrar historial de pacientes por habitación"+"\n"+"4.  Generar XML"+"\n"+"5. Salir";
+        return readShort(menuText);
+    }
+}

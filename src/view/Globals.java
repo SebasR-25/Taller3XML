9bbf6c33f293aecd5c251b5a1d955e6fd465bec2
SebasR-25 <seruprisatur2703@gmail.com>
2023-04-07 00:45:29 -0500

    Se agrega una font al Globals

--- src/view/Globals.java
+++ src/view/Globals.java
@@ -4,4 +4,5 @@ import java.awt.*;
 
 public class Globals {
     public static final Font OPTION_BUTTON_FONT = new Font("Arial", Font.BOLD, 20);
+    public static final Font FIELD_FONT = new Font("Arial", Font.PLAIN, 20);
 }

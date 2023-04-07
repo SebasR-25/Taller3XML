a493b830c4b69396344d2b639e5202587567ffdd
SebasR-25 <seruprisatur2703@gmail.com>
2023-04-07 00:41:44 -0500

    Se arreglan detalles en los textos

--- src/view/RoomPanel.java
+++ src/view/RoomPanel.java
@@ -29,13 +29,13 @@ public class RoomPanel extends JPanel {
 
     private void addComponents() {
         add(titleLabel, new GridBagConstraints(0, 0, 2, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-        add(new JLabel("<html><h2>Ingrese el Id: </h2></html>"), new GridBagConstraints(0, 1, 2, 1, 1, 1, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 50, 0, 0), 0, 0));
+        add(new JLabel("<html><h2>Ingrese el Id de la habitaci\u00f3n: </h2></html>"), new GridBagConstraints(0, 1, 2, 1, 1, 1, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 50, 0, 0), 0, 0));
         add(idTextField, new GridBagConstraints(0, 2, 2, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-        add(new JLabel("<html><h2>Ingrese el piso: </h2></html>"), new GridBagConstraints(0, 3, 2, 1, 1, 1, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(10, 50, 0, 10), 0, 0));
+        add(new JLabel("<html><h2>Ingrese el n\u00famero de piso: (N\u00fameros entre 1 y 30)</h2></html>"), new GridBagConstraints(0, 3, 2, 1, 1, 1, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(10, 50, 0, 10), 0, 0));
         add(floorTextField, new GridBagConstraints(0, 4, 2, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
         add(new JLabel("<html><h2>Ingrese el n\u00famero de la habitaci\u00f3n: </h2></html>"), new GridBagConstraints(0, 5, 2, 1, 1, 1, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(10, 50, 0, 10), 0, 0));
         add(roomNumberTextField, new GridBagConstraints(0, 6, 2, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-        add(new JLabel("<html><h2>Ingrese el n\u00famero de camas: </h2></html>"), new GridBagConstraints(0, 7, 2, 1, 1, 1, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(10, 50, 0, 10), 0, 0));
+        add(new JLabel("<html><h2>Ingrese el n\u00famero de camas: (N\u00famero entre 1 y 5)</h2></html>"), new GridBagConstraints(0, 7, 2, 1, 1, 1, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(10, 50, 0, 10), 0, 0));
         add(bedNumberTextField, new GridBagConstraints(0, 8, 2, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 10, 0), 0, 0));
         add(createRoomButton, new GridBagConstraints(0, 9, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(10, 0, 0, 50), 0, 0));
         add(backButton, new GridBagConstraints(1, 9, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(10, 50, 0, 0), 0, 0));
@@ -57,7 +57,7 @@ public class RoomPanel extends JPanel {
     }
 
     private void textFieldSetFeatures(JTextField textField) {
-        textField.setPreferredSize(new Dimension(300, 30));
+        textField.setPreferredSize(new Dimension(400, 30));
         textField.setFont(Globals.FIELD_FONT);
         ((AbstractDocument) textField.getDocument()).setDocumentFilter(new NumericDocumentFilter());
     }

90573c2f839540cca37f52d6fb919d74fe1cd02d
SebasR-25 <seruprisatur2703@gmail.com>
2023-04-06 22:22:43 -0500

    Se crea la vista principal (frame y panel)

--- src/view/Globals.java
+++ src/view/Globals.java
@@ -1,5 +1,7 @@
 package view;
 
-public class Globals {
+import java.awt.*;
 
+public class Globals {
+    public static final Font OPTION_BUTTON_FONT = new Font("Arial", Font.BOLD, 20);
 }
--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -1,7 +1,45 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
 
+    private MainPanel mainPanel;
+    public MainFrame() {
+        super("Menu");
+        setLayout(new GridBagLayout());
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            showErrorMessage(e.getMessage());
+        }
+        initComponents();
+        addComponents();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(700, 500);
+        setLocationRelativeTo(null);
+        setVisible(false);
+    }
+
+    private void addComponents() {
+        getContentPane().add(mainPanel);
+    }
+
+    private void initComponents() {
+        mainPanel = new MainPanel();
+    }
+
+    private void showErrorMessage(String message) {
+        JOptionPane.showMessageDialog(this, message, "Error", JOptionPane.ERROR_MESSAGE);
+    }
+
+    public void start() {
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.start();
+    }
 }

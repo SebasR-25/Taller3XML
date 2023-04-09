7ce3b43fba0f9524b7554c871c93d8793b8b4d08
SebasR-25 <seruprisatur2703@gmail.com>
2023-04-09 15:34:47 -0500

    Segundo commit de la clase de lectura, se ha avanzado en la lógica para los nodos de habitación

--- src/fileOperations/ReadXML.java
+++ src/fileOperations/ReadXML.java
@@ -2,15 +2,19 @@ package fileOperations;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+import model.Room;
 
 public class ReadXML {
+    public List<Room> readRooms;
     public void readXML() throws SAXException, ParserConfigurationException, IOException{
         File xmlFile = new File ("src/resources/Hospital.xml");
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -18,9 +22,12 @@ public class ReadXML {
         Document document = builder.parse(xmlFile);
         document.getDocumentElement().normalize();
 
-        NodeList list = document.getElementsByTagName("habitacion");
+        NodeList list = document.getElementsByTagName("room");
         for (int i = 0; i < list.getLength(); i++) {
-            Node habitacion = list.item(i);
+            Node room = list.item(i);
+            if (room.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) room;
+            }
         }
     }
 }

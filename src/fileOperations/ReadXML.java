b702f9f51a3d566a38d423a6db860f7efa943ae3
SebasR-25 <seruprisatur2703@gmail.com>
2023-04-09 16:50:52 -0500

    Se desarrolló gran parte de la clase de lectura de archivos

--- src/fileOperations/ReadXML.java
+++ src/fileOperations/ReadXML.java
@@ -11,10 +11,12 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+import model.Patient;
 import model.Room;
 
 public class ReadXML {
     public List<Room> readRooms;
+    
     public void readXML() throws SAXException, ParserConfigurationException, IOException{
         File xmlFile = new File ("src/resources/Hospital.xml");
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -24,10 +26,27 @@ public class ReadXML {
 
         NodeList list = document.getElementsByTagName("room");
         for (int i = 0; i < list.getLength(); i++) {
+            Room tempRoom = new Room();
             Node room = list.item(i);
             if (room.getNodeType() == Node.ELEMENT_NODE) {
                 Element element = (Element) room;
+                tempRoom.setId(Integer.parseInt(element.getAttribute("id")));
+                tempRoom.setFloorNumber(Integer.parseInt(element.getElementsByTagName("floorNumber").item(0).getTextContent()));   
+                tempRoom.setRoomNumber(Integer.parseInt(element.getElementsByTagName("roomNumber").item(0).getTextContent()));
+                
+                NodeList patientList = document.getElementsByTagName("patient");
+                for(int j = 0; i < patientList.getLength(); j++){
+                    Patient tempPatient = new Patient();
+                    Node patient = patientList.item(j);
+                    if(patient.getNodeType() == Node.ELEMENT_NODE){
+                        Element element2 = (Element) patient;
+                        tempPatient.setFirstName(element2.getElementsByTagName("firstName").item(0).getTextContent());
+                        tempPatient.setLastName(element2.getElementsByTagName("lastName").item(0).getTextContent());
+                        tempPatient.setContactPhoneNumber(element2.getElementsByTagName("contactPhoneNumber").item(0).getTextContent());
+                    }
+                    tempRoom.addPatient(tempPatient);
+                }
             }
         }
-    }
+    } 
 }

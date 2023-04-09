ddad593eaea5ad27a8530858fde25c97ff8dca47
SebasR-25 <seruprisatur2703@gmail.com>
2023-04-08 19:24:29 -0500

    Se empieza a desarrollar la clase para la lectura de archivos XML

--- src/fileOperations/ReadXML.java
+++ src/fileOperations/ReadXML.java
@@ -1,5 +1,26 @@
 package fileOperations;
 
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 public class ReadXML {
-    
+    public void readXML() throws SAXException, ParserConfigurationException, IOException{
+        File xmlFile = new File ("src/resources/Hospital.xml");
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(xmlFile);
+        document.getDocumentElement().normalize();
+
+        NodeList list = document.getElementsByTagName("habitacion");
+        for (int i = 0; i < list.getLength(); i++) {
+            Node habitacion = list.item(i);
+        }
+    }
 }

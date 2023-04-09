77e4a3611a9d88401029e4579f5a96ca9abb0075
SebasR-25 <seruprisatur2703@gmail.com>
2023-04-09 14:41:41 -0500

    Se agregan constructores a las clases Room y Patient

--- src/model/Room.java
+++ src/model/Room.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Room{
@@ -8,7 +9,23 @@ public class Room{
     private int id;
     private List<Patient> patients;
     private int roomNumber;
-    
+
+    public Room() {
+        bedNumber = 0;
+        floorNumber = 0;
+        id = 0;
+        patients = new ArrayList<>();
+        roomNumber = 0;
+    }
+
+    public Room(int bedNumber, int floorNumber, int id, List<Patient> patients, int roomNumber) {
+        this.bedNumber = bedNumber;
+        this.floorNumber = floorNumber;
+        this.id = id;
+        this.patients = patients;
+        this.roomNumber = roomNumber;
+    }
+
     public void addPatient(Patient patient){
         patients.add(patient);
     }
@@ -33,6 +50,11 @@ public class Room{
     public List<Patient> getPatients() {
         return patients;
     }
+
+    public void setPatients(List<Patient> patients) {
+        this.patients = patients;
+    }
+
     public int getRoomNumber() {
         return roomNumber;
     }

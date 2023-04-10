9ebcb7cbeed66621159fd125be09fcaa36850dc0
SebasR-25 <seruprisatur2703@gmail.com>
2023-04-10 08:10:24 -0500

    Se agrega la clase RoomManager para guardar la lista de habitaciones

---
+++ src/model/RoomManager.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RoomManager {
+    private List<Room> rooms;
+
+    public RoomManager() {
+        rooms = new ArrayList<>();
+    }
+
+}

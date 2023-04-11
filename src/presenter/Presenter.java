11db9c8026afa3f3198c5143adb8395cee94e9ee
SebasR-25 <seruprisatur2703@gmail.com>
2023-04-11 00:43:52 -0500

    Se agregó la característica de guardado de datos al cerrar el programa para mantener la persistencia de datos

--- src/presenter/Presenter.java
+++ src/presenter/Presenter.java
@@ -14,6 +14,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactoryConfigurationError;
 
 import org.xml.sax.SAXException;
 
@@ -28,62 +30,11 @@ public class Presenter implements ActionListener {
         roomManager = new RoomManager();
         readder = new ReadXML();
         writter = new WriteXML();
-        loadDefaultRooms();
-    }
-
-    private void loadDefaultRooms() {
-        List<Patient> patients = new ArrayList<>();
-        patients.add(new Patient("123456789", "Juan", "Perez", Status.ACTIVE));
-        patients.add(new Patient("987654321", "Maria", "Gomez", Status.ACTIVE));
-        patients.add(new Patient("123456789", "Pedro", "Gonzalez", Status.ACTIVE));
-        patients.add(new Patient("987654321", "Luis", "Rodriguez", Status.ACTIVE));
-        patients.add(new Patient("123456789", "Jose", "Martinez", Status.ACTIVE));
-        patients.add(new Patient("987654321", "Ana", "Lopez", Status.ACTIVE));
-        patients.add(new Patient("123456789", "Laura", "Gutierrez", Status.ACTIVE));
-        patients.add(new Patient("987654321", "Sofia", "Ramirez", Status.ACTIVE));
-        patients.add(new Patient("123456789", "Camila", "Sanchez", Status.ACTIVE));
-        patients.add(new Patient("987654321", "Valentina", "Torres", Status.ACTIVE));
-        patients.add(new Patient("123456789", "Isabella", "Diaz", Status.ACTIVE));
-        patients.add(new Patient("987654321", "Martina", "Perez", Status.ACTIVE));
-        patients.add(new Patient("123456789", "Antonia", "Gomez", Status.ACTIVE));
-        patients.add(new Patient("987654321", "Fernanda", "Gonzalez", Status.ACTIVE));
-        patients.add(new Patient("123456789", "Lucia", "Torres", Status.ACTIVE));
-        patients.add(new Patient("987654321", "Daniela", "Gutierrez", Status.ACTIVE));
-        patients.add(new Patient("123456789", "Mariana", "Lopez", Status.ACTIVE));
-        patients.add(new Patient("987654321", "Julieta", "Martinez", Status.ACTIVE));
-        patients.add(new Patient("123456789", "Valeria", "Sanchez", Status.ACTIVE));
-        patients.add(new Patient("987654321", "Sara", "Ramirez", Status.ACTIVE));
-        patients.add(new Patient("123456789", "Mia", "Rodriguez", Status.ACTIVE));
-        patients.add(new Patient("987654321", "Victoria", "Diaz", Status.ACTIVE));
-        patients.add(new Patient("123456789", "Nicole", "Perez", Status.ACTIVE));
-        patients.add(new Patient("987654321", "Renata", "Gomez", Status.ACTIVE));
-        patients.add(new Patient("123456789", "Constanza", "Gonzalez", Status.ACTIVE));
-        patients.add(new Patient("987654321", "Jimena", "Torres", Status.ACTIVE));
-        patients.add(new Patient("123456789", "Catalina", "Gutierrez", Status.ACTIVE));
-        patients.add(new Patient("987654321", "Paula", "Lopez", Status.ACTIVE));
-        patients.add(new Patient("123456789", "Isidora", "Martinez", Status.ACTIVE));
-        patients.add(new Patient("987654321", "Emilia", "Sanchez", Status.ACTIVE));
-        patients.add(new Patient("123456789", "Florencia", "Ramirez", Status.ACTIVE));
-        patients.add(new Patient("987654321", "Antonella", "Rodriguez", Status.ACTIVE));
-        patients.add(new Patient("123456789", "Javiera", "Diaz", Status.ACTIVE));
-        patients.add(new Patient("987654321", "Micaela", "Perez", Status.ACTIVE));
-        patients.add(new Patient("123456789", "Agustina", "Gomez", Status.ACTIVE));
-        ;
-        List<Room> rooms = new ArrayList<>();
-        rooms.add(new Room(5, 1, 1, List.of(patients.get(0), patients.get(1)), 101));
-        rooms.add(new Room(5, 1, 2, List.of(patients.get(2), patients.get(3)), 102));
-        rooms.add(new Room(5, 1, 3, List.of(patients.get(4), patients.get(5)), 103));
-        rooms.add(new Room(5, 1, 4, List.of(patients.get(6), patients.get(7)), 104));
-        rooms.add(new Room(5, 1, 5, List.of(patients.get(8), patients.get(9)), 105));
-
-        rooms.add(new Room(5, 2, 6, List.of(patients.get(10), patients.get(11), patients.get(11), patients.get(12)), 201));
-        rooms.add(new Room(5, 2, 7, List.of(patients.get(13), patients.get(14), patients.get(15), patients.get(16)), 202));
-        rooms.add(new Room(5, 2, 8, List.of(patients.get(17), patients.get(18), patients.get(19), patients.get(20)), 203));
-        roomManager.setRooms(rooms);
     }
 
     private void start() throws SAXException, ParserConfigurationException, IOException {
         readder.readXML();
+        roomManager.setRooms(readder.getReadRooms());
         view.start();
     }
 
@@ -114,8 +65,13 @@ public class Presenter implements ActionListener {
                 view.showErrorMessage("No implementado");
                 break;
                 case  "EXIT":
+                try {
+                    writter.writeXML(roomManager.getRooms());
+                } catch (ParserConfigurationException | TransformerFactoryConfigurationError | TransformerException e) {
+                    view.showErrorMessage("Se generó un error al escribir el archivo");
+                }
                 view.showSuccessMessage("Se cerró exitosamente el programa");
-                view.dispose();
+                System.exit(0);
                 break;
         }
     }
